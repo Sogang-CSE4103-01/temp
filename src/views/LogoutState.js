4739a7c0bf895128919606f4deaa6eed41ad46c2
fideliokim <rlaalsrb720@gmail.com>
2024-11-26 22:32:27 +0900

    logout final

--- src/views/LogoutState.js
+++ src/views/LogoutState.js
@@ -11,6 +11,7 @@ export const useLogOut = () => {
         setIsLoggedOut(true);
 
         console.log("logout requested");
+        console.log(username, password);
 
         setUsername('');
         setPassword('');
